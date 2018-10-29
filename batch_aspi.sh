cc12dd66a3a242fe5eded04e28508aa8fd634f19
Christian Zufferey <christian@zufferey.com>
2018-10-29 17:28:25 +0100

    - ajouté la partie echo screen pour le mode hors console

--- batch_aspi.sh
+++ batch_aspi.sh
@@ -1,7 +1,22 @@
 #!/usr/bin/env bash
 #petit script pour aspirer seulement le html d'une liste de sites web qui se trouvent dans un fichier .csv
-#zf181029.1717
+#zf181029.1727
 #source: https://www.cyberciti.biz/faq/unix-linux-bash-read-comma-separated-cvsfile/
+
+echo -e " 
+Afin de pouvoir garder batch_aspi en marche tout en pouvant quitter la console, il serait bien de le faire tourner dans un 'screen' avec:
+
+screen -S batch_aspi  pour entrer dans screen
+./batch_aspi.sh       pour lancer le serveur WEB dans screen
+CTRL+a,d              pour sortir de screen en laissant tourner le serveur
+screen -r batch_aspi  pour revenir dans screen
+screen -x batch_aspi  pour revenir à plusieurs dans screen
+CTRL+d                pour terminer screen
+screen -list          pour lister tous les screens en fonctionement
+"
+read -p "appuyer une touche pour démarrer..."
+
+
 
 echo ---------- start
 
