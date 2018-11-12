14bbf453b05f6f38f488ef6ea4d8c88b7d826ec4
Christian Zufferey <christian@zufferey.com>
2018-11-12 15:43:34 +0100

    - corrigé le script d'install avec la nouvelle lib open-cv contrib pour python

--- template_matching/install.sh
+++ template_matching/install.sh
@@ -2,36 +2,15 @@
 #Petit script pour installer tout ce qu'il faut pour OpenCV sur Python
 #ATTENTION: ça été fait pour une structure perso !
 #faudra modifier le script pour d'autres structures
-#zf181109.1032
+#zf181112.1542
 
-#source: faut regarder aussi ceci https://pypi.org/project/opencv-contrib-python/
+#source: https://pypi.org/project/opencv-contrib-python/
+#et la fin de ceci: http://answers.opencv.org/question/199318/how-to-use-sift-in-python/
 
 sudo apt update
-sudo apt install -y python-pip python-opencv python-tk
+sudo apt install -y python-pip python-tk pinta
 sudo -H python -m pip install -U pip
 sudo -H python -m pip install -U matplotlib
+sudo -H pip install opencv-contrib-python==3.4.2.17
 
-
-exit
-
-
-
-
-#test si l'argument est vide
-if [ -z "$1" ]
-  then
-    echo -e "\nSyntax: ./aspi.sh site_name user passwd \n\n"
-    exit
-fi
-
-echo ---------- start aspi.sh
-
-site=$1
-agent="Mozilla/5.0"
-
-mkdir html
-cd html
-#wget --user-agent="Mozilla/5.0" -E -m -e robots=off –w 10 --no-parent -X "/files,/templates,/cms/engineName" "$site"
-wget -a log.txt --user-agent="Mozilla/5.0" -k -p --no-parent -T 1 -t 1 "$site"
-#echo $site
 
--- template_matching/start.sh
+++ template_matching/start.sh
@@ -2,47 +2,14 @@
 #Petit script pour lancer le binz
 #ATTENTION: ça été fait pour une structure perso !
 #faudra modifier le script pour d'autres structures
-#zf181107.1406
+#zf181112.1541
 
 #source: 
+#note: convert ../../../imgdiff/copyscreen_sans.png -fx '(r+g+b)/3' -colorspace Gray copyscreen_sans.jpg
+
 
 python test1.py 
 
 
 
-
-
-exit
-
-
-
-sudo apt update
-sudo apt -y install python-pip
-sudo apt install -y python-opencv
-sudo python -m pip install -U pip
-sudo python -m pip install -U matplotlib
-
-
-exit
-
-
-
-
-#test si l'argument est vide
-if [ -z "$1" ]
-  then
-    echo -e "\nSyntax: ./aspi.sh site_name user passwd \n\n"
-    exit
-fi
-
-echo ---------- start aspi.sh
-
-site=$1
-agent="Mozilla/5.0"
-
-mkdir html
-cd html
-#wget --user-agent="Mozilla/5.0" -E -m -e robots=off –w 10 --no-parent -X "/files,/templates,/cms/engineName" "$site"
-wget -a log.txt --user-agent="Mozilla/5.0" -k -p --no-parent -T 1 -t 1 "$site"
-#echo $site
 
