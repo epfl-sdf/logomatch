63351985b59604fb1cfa4f5242815073ae7c9394
Christian Zufferey <christian@zufferey.com>
2018-11-13 11:26:33 +0100

    - commencé à tout refactoriser. Donc plus rien ne fonctionne !

---
+++ phantomjs/start.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+#Petit script pour lancer le binz
+#ATTENTION: ça été fait pour une structure perso !
+#faudra modifier le script pour d'autres structures
+#zf181113.1114
+
+#source: 
+#note: convert ../../../imgdiff/copyscreen_sans.png -fx '(r+g+b)/3' -colorspace Gray copyscreen_sans.jpg
+
+phantomjs test5.js www.epfl.com
+
+
+
+
