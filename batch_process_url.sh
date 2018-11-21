179e2e6fe00f2959ab97c58dd93ad1da3596760c
Christian Zufferey <christian@zufferey.com>
2018-11-21 14:15:29 +0100

    - splité batch_process_url.sh afin de pouvoir tester qu'un seul site à la mano - par encore testé !

--- batch_process_url.sh
+++ batch_process_url.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 #petit script pour faire le preprocessing d'une liste de sites web qui se trouvent dans un fichier .csv
 #afin d'épurer la liste brute d'url
-#zf181120.1647
+#zf181121.1413
 
 #source: https://www.cyberciti.biz/faq/unix-linux-bash-read-comma-separated-cvsfile/
 
@@ -31,8 +31,8 @@ cp /dev/null redir.log
 cp /dev/null dyna.log
 cp /dev/null ./data/liste_url.csv
 
-#INPUT=./data/liste_sites.csv
-INPUT=./data/urls_test2.csv
+INPUT=./data/liste_sites.csv
+#INPUT=./data/urls_test2.csv
 
 
 OLDIFS=$IFS
@@ -48,14 +48,9 @@ while read name ip ; do
         site=$name".epfl.ch"
         echo -e "site: "$site
 
-
         url="http://"$site
-        url=`./loop_url_process_header.sh $url`                 #test les redirections et les erreurs dans le header
+        url=`./process_header.sh $url` 
 
-        if [ "$url" != "" ]
-        then
-            url=`./url_process_html.sh $url`                    #test les redirections dans le html
-        fi
 
         if [ "$url" != "" ]
         then
@@ -63,23 +58,9 @@ while read name ip ; do
             echo $url >> ./data/liste_url.csv                   #sauve l'url finale à scanner
         fi
 
-
 	fi
 	((nblines+=1))
 	echo ""
 done < $INPUT
 IFS=$OLDIFS
-
-
-exit
-
-
-
-
-
-echo -e "
-il y a comme nombre de pages HTML:
-"
-
-find ./html |grep '\.html' |wc
 
---
+++ process_url.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+#petit script pour faire le preprocessing d'un site web 
+#afin d'épurer les sites à scanner par la suite
+#zf181121.1410
+
+#source: https://www.cyberciti.biz/faq/unix-linux-bash-read-comma-separated-cvsfile/
+
+url=$1
+
+url=`./loop_url_process_header.sh $url`                 #test les redirections et les erreurs dans le header
+
+if [ "$url" != "" ]
+then
+    url=`./url_process_html.sh $url`                    #test les redirections dans le html
+fi
+
+if [ "$url" != "" ]
+then
+    echo -e "url: "$url
+    echo $url
+fi
+
