b0b864722e1b463a06274e04fa9b216cc368b609
Giovanni Cangiani <giovanni.cangiani@epfl.ch>
2018-11-28 12:44:44 +0100

    Extract run parameters and fix new dir structure
    Run parameters are now in a separate file 
    (default: start.conf) which can be passed as parameter.
    This way we avoid modifying version controlled 
    script file.
    
    Also fix compatibility with new directory structure

--- feature_matching/start.sh
+++ feature_matching/start.sh
@@ -1,116 +1,121 @@
 #!/usr/bin/env bash
-# This scruot executes test6.py up to three times. 
-# The first time on all the pages of the provided directory;
-# then only on the "maybe" pages from the previous step.
-# It accepts two mandatory argument:
-#   1. the path of the directory containing the images
-#   2. the path of the output directory 
-# an optional third argument that is the python exec.
-# The output is a directory (e.g. pippo) and a file (e.g. pippo.out).
-# 
-# Example:
-# start.sh pages/test attempt1
-# start.sh pages/test attempt2 python3
-# start.sh pages/test attempt3 "docker run -it -v $PWD:/wd opencv"
-#
-#gc181127.1625
+#gc181128.1125
 
-if [ -z "$1" ]
-  then
-    echo -e "\nSyntax: ./start.sh source_folder destination_folder \n\n"
+python="python3"
+confile="start.conf"
+
+usage() {
+  cat | sed 's/    //' << __EOF 
+
+    This script executes feature_matcher.py one or more times. 
+    The first time on all the pages of the provided directory;
+    then only on the "maybe" pages from the previous step.
+    It accepts two mandatory argument:
+      1. the path of the directory containing the images
+      2. the path of the output directory
+    The output is a directory (e.g. pippo) and a file (e.g. pippo.out).
+
+    Optional argument:
+      -h             Show usage and exit
+      -p PYTHON      Specify which python exec to use. Example:
+                     -p python
+                     -p "docker run -it -v $PWD:/wd opencv"
+      -f             Read the run descriptions from an external file instead of
+                     using built in ones. The default is start.conf
+                     The config file have one line per feature_matcher run
+                     and each line is just a list of arguments for feature_matcher.
+
+__EOF
+}
+
+while getopts ":hp:f:" OPT; do
+  [[ $OPTARG =~ ^- ]] && die "Option -$OPT requires an argument."
+  case $OPT in
+    :)
+      die "Option -$OPTARG requires an argument."; ;;
+    h)
+      usage; exit; ;;
+    p)
+      python="$OPTARG"; ;;
+    f)
+      confile="$OPTARG"; ;;
+  esac
+done
+shift $((OPTIND-1))
+
+if [ -z "$1" -o -z "$2" ] ; then
+    usage
     exit
 fi
 
-pd=$1 
+pd=$1
 od=$2
-if [ -n "$3" ] ; then
-  python="$3"
-else
-  python="python3"
-fi
-echo "Using $python"
+
+# Read run parameters from config file
+IFS=$'\r\n' GLOBIGNORE='*' command eval "configs=(\$(grep -v '#' $confile))"
+
+echo "pd:       $pd" >&2
+echo "od:       $od" >&2
+echo "confile:  $confile" >&2
+i=0
+while [ "x${configs[i]}" != "x" ] ; do
+  cfg="${configs[i]}"
+  let i=$i+1
+  echo "  run $i: ${cfg}" >&2
+done
 
 # ---------------------------------------------------------------- 0 setup
-# Generate logos from svg
-./generate.sh
-
-lg=logo/png
-
-o1=$od/m1
-o2=$od/m2
-o3=$od/m3
-
 # TODO: fix for absolute paths
-apd=$PWD/$pd
+apd=$(pwd)/$pd
+
+# Generate logos from svg
+./generate_logos.sh logos
 
 if [ -d $od ] ; then
   echo "Output directory $od already exists. Please delete it and try again or change name"
   exit 1
 fi
+
 mkdir -p $od || (echo "Could not create output dir"; exit 1)
 
 # Copy this file for future reference 
-cp $0 $od/
+cp $confile $od/start.conf
 
-# ------------------------------------------------------------------- 1
-# First run without special treatments and large "maybe" zone
-echo "$(date +%F-%R:%S) Start first round" >&2
-$python test6.py -K 5 -l 0.68 -n 4 -y 10 -m $o1 $pd $lg > ${o1}.out
-if [ "$?" != "0" ] ; then
-  echo "Test failed. Stopping."
-  exit
-fi
-cat $o1.out > $od.out
-# exit  # uncomment to stop here
+# ----------------------------------------------------------------- 1 RUN
 
+all_out=""; out=""; m=""
+let i=0
+while [ "x${configs[i]}" != "x" ] ; do
+  cfg="${configs[i]}"
+  let i=$i+1
 
+  # Create a new set of pages with the maybe pages of last run
+  if [ -n "$m" -a -n "$out" ] ; then
+    pages=${m}_maybe
+    [ -d $pages ] || mkdir $pages
+    awk '/maybe$/{print $1;}' $out | while read p ; do
+      ln -s $apd/$p.png $pages/
+    done
+  else
+    pages=$pd
+  fi
 
-# ------------------------------------------------------------------- 2
-# Create a new set of pages with the maybe pages
-[ -d ${o1}_maybe ] || mkdir ${o1}_maybe
-awk '/maybe$/{print $1;}' $o1.out | while read p ; do
-  ln -s $apd/$p.png ${o1}_maybe/
+  m=$od/m$i
+  out=$m.out
+  all_out="$all_out $out"
+
+  echo "$(date +%F-%R:%S) Start round $i" >&2
+  echo "$python feature_matcher.py $cfg -m $m $pages logos > $out" >&2
+  $python feature_matcher.py $cfg -m $m $pages logos > $out
+  if [ "$?" != "0" ] ; then
+    echo "Test failed. Stopping."
+    exit
+  fi
+
+  # Each loop overwrites the output of the previous one as it is cumulative
+  cat $all_out | grep -e 'yes$'   >  $od.out
+  cat $all_out | grep -e 'no$'    >> $od.out
+  cat $out     | grep -e 'maybe$' >> $od.out
 done
 
-# Run again on maybe-pages with some tweaks:
-# -k correct thresholds based on number of keypoints in the logo
-# -D discard points that are more than given value (180) pixels far from the center of mass
-# -l relax a bit the Lowe cryterium for match seleciton 
-echo "$(date +%F-%R:%S) Start second round" >&2
-
-# $python test6.py -k --mingdisp 40 --maxgdisp 160 -l 0.74 -n 5 -y 8 -m $o2 ${o1}_maybe $lg > $o2.out
-# $python test6.py -p 8 -K 5 --mingdisp 40 --maxgdisp 160 -l 0.70 -n 6 -y 12 -m $o2 ${o1}_maybe $lg > ${o2}.out
-$python test6.py --maxgdisp 160 -p 8 -K 5 -l 0.70 -n 4 -y 4 -m $o2 ${o1}_maybe $lg > ${o2}.out
-
-
-
-if [ "$?" != "0" ] ; then
-  echo "Test failed. Stopping."
-  exit
-fi
-
-cat $o1.out $o2.out | grep -e 'yes$' >  $od.out
-cat $o1.out $o2.out | grep -e 'no$'  >> $od.out
-cat $o2.out | grep -e 'maybe$'       >> $od.out
-exit # uncomment to stop here
-
-
-
-# ------------------------------------------------------------------- 3
-[ -d ${o2}_maybe ] || mkdir ${o2}_maybe
-awk '/maybe$/{print $1;}' $o2.out | while read p ; do
-  ln -s $apd/$p.png ${o2}_maybe/
-done
-
-# Run again on maybe-pages with some tweaks
-# -p split the image in three parts if score is smaller than given value (8)
-echo "$(date +%F-%R:%S) Start third round" >&2
-$python test6.py -p 8 -k --mingdisp 40 --maxgdisp 160 -l 0.74 -n 6 -y 8 -m $o3 ${o2}_maybe $lg > ${o3}.out
-if [ "$?" != "0" ] ; then
-  echo "Test failed. Stopping."
-  exit
-fi
-
-cat $o1.out $o2.out $o3.out | grep -e 'yes$' >  $od.out
-cat $o1.out $o2.out $o3.out | grep -e 'no$'  >> $od.out
-cat $o3.out | grep -e 'maybe$'               >> $od.out
+exit
--- feature_matching/test.sh
+++ feature_matching/test.sh
@@ -1,36 +1,63 @@
 #!/bin/bash
 # This script executes start.sh on all the test pages, then make a summary of the result
 
-if [ -z "$1" ]
-  then
-    echo -e "\nSyntax: ./test.sh destination_folder [python command]\n\n"
-    exit
-fi
-match=$1
+usage() {
+  cat | sed 's/    //' << __EOF 
 
-if [ -n "$2" ] ; then
-  python="$2"
-else
-  python=""
-fi
+    This script executes start.sh for every positional argument
+    interpreted as the basename of a XXX.conf file that will 
+    run start.sh with pages/test as pages, logos as logos,
+    XXX.conf as configuration file, and XXX as output directory
 
-out=${match}_test.out
+    Optional argument:
+      -h             Show usage and exit
+      -p PYTHON      Specify which python exec to use. Example:
+                     -p python
+                     -p "docker run -it -v $PWD:/wd opencv"
 
-echo "-----------------------------------------------" >> $out
-cat start.sh                                           >> $out
-echo "-----------------------------------------------" >> $out
-date                                                   >> $out
-./start.sh pages/test $match "$python"
-cat ${match}.out                                       >> $out
-date                                                   >> $out
-echo "-----------------------------------------------" >> $out
-
-awk '
-BEGIN{n_good=0; n_bad=0; n_mb=0;}
-/maybe$/{n_mb=n_mb+1; next;}
-/^yes_.*no$|^no_.*yes$/{n_bad=n_bad+1; next;}
-/^yes_.*yes$|^no_.*no$/{n_good=n_good+1; next;}
-END{
-  printf("good: %d  %5.1f     bad: %d  %5.1f     mb: %d. %5.1f\n\n", n_good, 100*n_good/NR, n_bad, 100*n_bad/NR, n_mb, 100*n_mb/NR);
+__EOF
 }
-' $match.out                                           >> $out
+
+python="python3"
+while getopts ":hp:" OPT; do
+  [[ $OPTARG =~ ^- ]] && die "Option -$OPT requires an argument."
+  case $OPT in
+    :)
+      die "Option -$OPTARG requires an argument."; ;;
+    h)
+      usage; exit; ;;
+    p)
+      python="$OPTARG"; ;;
+  esac
+done
+shift $((OPTIND-1))
+
+if [ ! -d pages/test ] ; then
+  mkdir -p pages/test && rsync -av ../test_pages/ pages/test/
+fi
+
+while [ $# -gt 0 ] ; do
+  cf=$(basename $1 .conf)
+  shift 1
+  out=${cf}_test.out
+  echo "-----------------------------------------------" >> $out
+  cat $cf.conf                                           >> $out
+  echo "-----------------------------------------------" >> $out
+  date                                                   >> $out
+
+  ./start.sh -p "$python" -f ${cf}.conf pages/test $cf
+
+  cat $cf.out                                            >> $out
+  date                                                   >> $out
+  echo "-----------------------------------------------" >> $out
+
+  awk '
+  BEGIN{n_good=0; n_bad=0; n_mb=0;}
+  /maybe$/{n_mb=n_mb+1; next;}
+  /^yes_.*no$|^no_.*yes$/{n_bad=n_bad+1; next;}
+  /^yes_.*yes$|^no_.*no$/{n_good=n_good+1; next;}
+  END{
+    printf("good: %d  %5.1f     bad: %d  %5.1f     mb: %d. %5.1f\n\n", n_good, 100*n_good/NR, n_bad, 100*n_bad/NR, n_mb, 100*n_mb/NR);
+  }
+  ' $cf.out                                              >> $out
+done
