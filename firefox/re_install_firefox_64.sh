33941c05200c7bb85fe911a6cba5a04ffa8bdd7b
Christian Zufferey <christian@zufferey.com>
2019-02-26 17:50:02 +0100

    Corriger un bug pour installer le Firefox version 64.x et modifié screen_copy pour qu'il utilise firefox en mode headless au lieu de phantomjs pour les screencopy

--- firefox/re_install_firefox_64.sh
+++ firefox/re_install_firefox_64.sh
@@ -1,10 +1,15 @@
 #Petit script pour installer K'ancienne version de Firefox 64
-#zf190226.0840
+#zf190226.1745
+
+#si une fois il n'est plus possible d'installer firefox sur la machine, c'est qu'il y a DEUX firefox installés partiellement en MEME temps !
+#on peut les détecter avec: 'sudo apt list --installed |grep firefox' et les désintaller un après l'autre avec: 'sudo apt remove appname'
+
 
 firefox --version
-sudo apt-get update && apt-get install -yq wget zip firefox
+sudo apt-get update && sudo apt-get install -yq wget zip firefox
 sudo apt -yq remove firefox
 sudo rm -rf /opt/firefox/ /etc/firefox/
+rm firefox-mozilla*.deb
 wget https://netcologne.dl.sourceforge.net/project/ubuntuzilla/mozilla/apt/pool/main/f/firefox-mozilla-build/firefox-mozilla-build_64.0.2-0ubuntu1_amd64.deb
 sudo apt install -yq ./firefox-mozilla-build_64.0.2-0ubuntu1_amd64.deb
 ./restore_firefox_config.sh
