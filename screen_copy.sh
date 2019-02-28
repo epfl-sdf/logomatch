31df89340279163015fcf19805478f73d5b3bdb9
Christian Zufferey <christian@zufferey.com>
2019-02-28 18:02:44 +0100

    Amélioré le screen copy avec Firefox en pensant avoir corrigé le problème de plantée au bout d'un certain temps

--- screen_copy.sh
+++ screen_copy.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 #petit script pour faire une copie d'écran de la homes pages d'un sites web
-#zf190226.1742
+#zf190228.1801
 
 #source: https://www.cyberciti.biz/faq/unix-linux-bash-read-comma-separated-cvsfile/
 
 
 #phantomjs --ignore-ssl-errors=true phantomjs_screen_copy.js $1 $2                #>/dev/null
-timeout 10 firefox --headless --screenshot $2 $1                #>/dev/null
+timeout 10 firefox --new-instance --headless --screenshot $2 $1                #>/dev/null
+sleep 1
+
