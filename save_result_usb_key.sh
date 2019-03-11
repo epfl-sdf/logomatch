2432e3d4af1ae17e9c8142035cd5f7dc32fb245b
Christian Zufferey <christian@zufferey.com>
2019-03-11 15:39:08 +0100

    Modifié le script de backup des résultats

--- save_result_usb_key.sh
+++ save_result_usb_key.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 #petit script pour faire faire une copie des images de résultat sur la clef USB 
-#zf190301.0928
+#zf190311.1538
 
 #source: 
 
 #rsync -n -r -v -t --progress --stats ./images2/* zuzu@siipc6.epfl.ch:/Volumes/logomatch/181122.1400
 #rsync -n -r -v -t --progress --stats ./images2/* zuzu@siipc6.epfl.ch:/Volumes/logomatch/181122.1400
-rsync -n -r -v -t --progress --stats ~/logomatch/images.190301.0853/* zuzu@siipc6.epfl.ch:/Volumes/LOGOMATCH/180103.0928
+rsync -n -r -v -t --progress --stats ~/logomatch/images.190301.0853/* zuzu@siipc6.epfl.ch:/Volumes/LOGOMATCH/180103.0928/images
+rsync -n -r -v -t --progress --stats /home/zuzu/logomatch/result/* zuzu@siipc6.epfl.ch:/Volumes/LOGOMATCH/180103.0928/result
+rsync -n -r -v -t --progress --stats /home/zuzu/logomatch/result/* zuzu@siipc6.epfl.ch:"/Users/zuzu/VirtualBox\ VM\ Masters/No\ zlightbackup/logomatch/result"
 
 
