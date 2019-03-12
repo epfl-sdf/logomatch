4ebb48697beee4050d187874700f04d780ae1d81
Christian Zufferey <christian@zufferey.com>
2019-03-12 14:22:35 +0100

    Ajouté la sauvegarde des images sur une machine remote

--- save_result_usb_key.sh
+++ save_result_usb_key.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #petit script pour faire faire une copie des images de résultat sur la clef USB 
-#zf190311.1538
+#zf190312.1420
 
 #source: 
 
@@ -9,5 +9,6 @@
 rsync -n -r -v -t --progress --stats ~/logomatch/images.190301.0853/* zuzu@siipc6.epfl.ch:/Volumes/LOGOMATCH/180103.0928/images
 rsync -n -r -v -t --progress --stats /home/zuzu/logomatch/result/* zuzu@siipc6.epfl.ch:/Volumes/LOGOMATCH/180103.0928/result
 rsync -n -r -v -t --progress --stats /home/zuzu/logomatch/result/* zuzu@siipc6.epfl.ch:"/Users/zuzu/VirtualBox\ VM\ Masters/No\ zlightbackup/logomatch/result"
+rsync -n -r -v -t --progress --stats /home/zuzu/logomatch/images.190301.0853/* zuzu@siipc6.epfl.ch:"/Users/zuzu/VirtualBox\ VM\ Masters/No\ zlightbackup/logomatch/images"
 
 
