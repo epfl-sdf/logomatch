9108741f7f67e63bea6315919e1c1b6dda88bacb
Christian Zufferey <christian@zufferey.com>
2019-03-26 15:21:58 +0100

    Corrigé un bug au niveau de l'entête du fichier CSV

--- sort_url.sh
+++ sort_url.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 #petit script pour faire trier et supprimer les doublons
-#zf181122.1048
+#zf190326.1444
 
 #source: 
 
+echo -e "site,url" > data/liste_url_unique.csv
 echo -e "Avant: "`cat data/liste_url.csv |wc -l`
-sort -u data/liste_url.csv > data/liste_url_unique.csv
+sort -u data/liste_url.csv >> data/liste_url_unique.csv
 echo -e "Après: "`cat data/liste_url_unique.csv |wc -l`
